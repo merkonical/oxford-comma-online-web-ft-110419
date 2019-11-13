6d44073e617091f11c9e71654b6853aec3b6108f
Maverick Adolfo <merkonical@gmail.com>
2019-11-13 05:48:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
+require 'pry'
 def oxford_comma(array)
-
+  if array.count < 2 
+    array.join
+  elsif array.count == 2 
+    array.join(" and ")
+  else 
+    last_word = array.pop
+    first_words = array.join(", ")
+    first_words + ", and " + last_word
+    binding.pry 
+  end 
 end
